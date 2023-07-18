80c43e25b99eba7508074d3a9edb4a021530a704
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-18 11:31:53 +0300

    sfsd

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,9 +1,6 @@
 import random
-
 number = random.randint(-10000, 10000)
-
 last_digit = abs(number) % 10
-
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5\n".format(number, last_digit))
 elif last_digit == 0:
