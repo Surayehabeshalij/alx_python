b8c783d8f9482d2bc830f51d39f70810b7baa6b6
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-19 10:57:11 +0300

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:d}{:d}, ".format(i, j), end="")
+        if j==10:
+           print("{:d}{:d} ".format(i, j), end="")    
+            
+        else :
+           print("{:d}{:d}, ".format(i, j), end="") 
 
