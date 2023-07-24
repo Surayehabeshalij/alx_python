5c7369fd70a1daf6b5d26b08562243e7e54b7869
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-24 00:59:57 -0700

    check

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+# import the add function from add_0.py
+from add_0 import add
+# assign values to a and b
+a = 1
+b = 2
+
+# print the result of the addition
+print('{} + {} = {}'.format(a, b, add(a, b)))
