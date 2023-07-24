8d30fc1c421cf330e87b0c54831e044bf203a757
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-24 14:19:50 +0300

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    arg=sys.argv
+    arg = sys.argv
     size= len(arg)
     if size>1:
         print("{} arguments:".format(size))
-        for i in range(size):
+        for i in range(size) :
             print("{}: {}".format(i+1, arg[i]))
     elif size == 0:
         print("{} arguments:".format(size))
-    elif size ==1:
+    elif size == 1:
         print("1 argument:")
         print("1: {}".format(arg[0]))
-    
