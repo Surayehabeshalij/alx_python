1cc1d04f153872c48a3073c931c4fb0f69dffc88
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-24 02:10:26 -0700

    xz

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 # import the add function from add_0.py
-from add_0 import add
+
 if __name__ == '__main__':
+    from add_0 import add
+
     a = 1
     b = 2
     result = add(a, b)
