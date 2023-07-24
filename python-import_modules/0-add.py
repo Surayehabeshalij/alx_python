8c16605676571279f048dd448892fb4d6071f2ca
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-24 13:39:58 +0300

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 # import the add function from add_0.py
-
+from .add_0 import add
 if __name__ == '__main__':
-    from .add_0 import add
+    
     a = 1
     b = 2
     result = add(a, b)
