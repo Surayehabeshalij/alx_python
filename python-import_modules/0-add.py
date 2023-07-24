855638e1598e2574ff5da7c29cfaa43be559d98d
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-24 13:41:50 +0300

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     
     a = 1
     b = 2
-    result = add(a, b)
     
+    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
 # print the result of the addition
-print('{:d} + {:d} = {:d}'.format(a, b, result))
+
