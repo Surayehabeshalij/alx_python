607d73fbb4ac1b35bd4dec9c6b8dd20120e77b94
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-24 13:34:17 +0300

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 # import the add function from add_0.py
-from add_0 import add
 
-
-a = 1
-b = 2
-result = add(a, b)
+if __name__ == '__main__':
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
     
 # print the result of the addition
 print('{:d} + {:d} = {:d}'.format(a, b, result))
