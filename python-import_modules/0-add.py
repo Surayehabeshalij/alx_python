a7b9296c945b59c5841831372f48d624c6caee49
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-24 01:33:28 -0700

    sgdf

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 # import the add function from add_0.py
 from add_0 import add
-
-# assign values to a and b
-a = 1
-b = 2
-
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    result = add(a, b)
+    
 # print the result of the addition
 print('{} + {} = {}'.format(a, b, add(a, b)))
