43cda2d9f1f9e29c976fc8601d04289d92fbfabd
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-25 11:08:05 +0300

    Update 5-raise_exception_msg.py

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-<<<<<<< HEAD
-    raise NameError("Error: {}".format(message))
-=======
     raise NameError("{}".format(message))
->>>>>>> 741086dfd8a74738b8f8692af785eb29fd8a4448
+
