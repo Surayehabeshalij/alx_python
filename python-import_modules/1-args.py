e54b927821739f9f318b58e1419a2482484a0071
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-25 11:34:35 +0300

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
-    
-    try :
-        import sys
-        arg = (sys.argv)[1:]
-        #arg = input().split()
-        size = len(arg)
-        if size > 1:
-            print("{} arguments:".format(size))
-            for i in range(size):
-                print("{}: {}".format(i+1, arg[i]))
-        elif size == 1:
-            print("1 argument:")
-            print("1: {}".format(arg[0]))
-    except EOFError :
+    import sys
+    arg = (sys.argv)[1:]
+    #arg = input().split()
+    size = len(arg)
+    if size > 1:
+        print("{} arguments:".format(size))
+        for i in range(size):
+            print("{}: {}".format(i+1, arg[i]))
+    elif size == 1:
+        print("1 argument:")
+        print("1: {}".format(arg[0]))
+    else :
         print("0 arguments.")
     
