bef1d3437d249823e4b4b9f64f42839be150dfcb
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-25 09:04:10 +0300

    Update 5-raise_exception_msg.py

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    raise NameError("Error: {}".format(message))
+    x = raise NameError("Error: {}".format(message))
+    return x
 try:
     raise_exception_msg("C is fun")
 except NameError as ne:
