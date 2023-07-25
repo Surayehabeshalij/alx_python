a80bd53519e42324ed41e1fe281bb9c8a366186c
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-25 11:30:52 +0300

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
     
     try :
         import sys
-        arg = sys.argv
+        arg = str(sys.argv)
         #arg = input().split()
         size = len(arg)
         if size > 1:
