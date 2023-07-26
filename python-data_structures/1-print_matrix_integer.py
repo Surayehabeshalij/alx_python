991d8485328b8087c74cf2b6cc0c6eec5beba1e4
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-26 10:17:40 +0300

    Update 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -2,7 +2,7 @@ def print_matrix_integer(matrix=[[]]):
     # Iterate over each row in the matrix
     for i, row in enumerate(matrix):
         # Iterate over each element in the row and print it with a space character at the end
-        print ("{:d}".format(row.join(" ")))
+        print ("{:d}".format(" ".join(row)))
         # If this is not the last row, print a newline character
         if i < len(matrix) - 1:
             print()
