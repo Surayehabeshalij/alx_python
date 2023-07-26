e05eb5e8aa970b5f79e9c90349aebc32cbcb77cb
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-26 09:39:04 -0700

    here

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,18 +2,12 @@ def best_score(a_dictionary):
     # Initialize the best key and value to None
     best_key = None
     best_value = None  
-    my_dict = None
-
     # Loop through each key-value pair in the dictionary
     for key, value in a_dictionary.items():
         # If the value is greater than the current best value, update the best key and value
         if best_value is None or value > best_value:
             best_key = key
             best_value = value
-        else:
-            best_key = best_score(my_dict)
-            print("Best: {}".format(best_key))
-    
     # Return the best key
     return best_key
     
