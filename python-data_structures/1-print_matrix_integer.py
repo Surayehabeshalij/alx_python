6763c4f42b326322341f3b3660ed8ee98e488d75
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-26 10:03:18 +0300

    Update 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         # Iterate over each element in the row and print it
         for element in row:
             # Use str.format() to print the integer with a width of 2 characters
-            print("{:d}".format(element), end="")
+            print("{:d}".format(element), end=" ")
         # Print a newline character after each row
         print()
