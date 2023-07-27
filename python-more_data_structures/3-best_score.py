6a4a3436666c1e1d36a730a0aa4f54d1a11119ab
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-27 08:46:21 +0300

    Update 3-best_score.py

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,17 +1,11 @@
 def best_score(a_dictionary):
     # Initialize the best key and value to None
     best_key = None
-    best_value = None  
-    # Loop through each key-value pair in the dictionary
-    for key, value in a_dictionary.items():
-        # If the value is greater than the current best value, update the best key and value
-        if best_value is None or value > best_value:
-            best_key = key
-            best_value = value
+    best_value = None 
     # Return the best key
-        elif a_dictionary is None:
+    if a_dictionary is None:
            return None
-        best_key = None
+    best_key = None
     for key, value in a_dictionary.items():
         if best_key is None or value > a_dictionary[best_key]:
             best_key = key
