030c52ac0d0dc516c33a80f4b54ef8933b6a6339
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-30 01:10:07 -0700

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,6 @@
 class Square:
     def __init__(self, size):
-        self.__size = size    
-    def area(self):
+        self.__size = size
+    def area (self):
         return self.__size ** 2
-print(Square.area.__doc__)
+print (Square.area.__doc__)
