95e92a08d11ff53dbfe0876a8f5d3cc8174e077b
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-30 02:39:28 -0700

    km

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -4,3 +4,7 @@ class Square:
     self.__size = size
   def area(self):
         return self.__size ** 2
+  def perimeter(self):
+        return 4 * self.__size
+  def __str__(self):
+        return f"Square({self.__size})"
