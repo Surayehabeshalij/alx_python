b036c2e4a22d217a829cde6f5939a5875895c30e
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-30 02:01:32 -0700

    zxcs

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -3,5 +3,12 @@ class Square:
         self.__size = size
     def area(self):
         return self.__size ** 2
-    def dict_(self):
-        return {'size': self.__size}
+    def dict_(self):    
+        mysquare = Square(3)
+        print(type(mysquare))  # <class 'square.Square'>
+        print(mysquare.dict_())  # {'size': 3}
+        try:
+            print(mysquare._size)
+        except Exception as e:
+            print(e)  
+                   
