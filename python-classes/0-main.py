7e44493036d218994c2628888dc119b80288afa6
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-30 01:50:48 -0700

    csc

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -2,4 +2,6 @@ class Square:
     def __init__(self, size):
         self.__size = size
     def area(self):
-       return self.__size ** 2
+        return self.__size ** 2
+    def dict_(self):
+        return {'size': self.__size}
