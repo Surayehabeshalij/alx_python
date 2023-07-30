f9e79aa12b3f5f715db84d83490057c0fd8eae03
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-30 02:33:04 -0700

    erf

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,4 @@
+#!/usr/bin/python3
 class Square:
-    def __init__(self, size):
-        self.__size = size   
-    def area(self):
-        return self.__size ** 2
-    print(Square.area.__doc__)
-    def dict_(self):    
-        mysquare = Square(3)
-        print(type(mysquare))  # <class 'square.Square'>
-        print(mysquare.dict_())  # {'size': 3}
-        try:
-            print(mysquare._size)
-        except Exception as e:
-            print(e)                 
+  def __init__(self, size):
+    self.__size = size
