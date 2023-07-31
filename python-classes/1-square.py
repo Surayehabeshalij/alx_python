60558936e0734e4a6c56b2aa93a491307cb67207
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-07-31 14:14:42 +0300

    Update 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -6,3 +6,9 @@ class Square:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
+    __doc__ = """
+    this is doc for class
+    """
+__doc__ = """
+    this is doc for module
+    """
