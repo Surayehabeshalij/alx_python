dbdaeb7b47537f807d49a27924a8930d2375e2ab
Surayehabeshalij <surafelshemsu@gmail.com>
2023-07-31 15:54:29 +0300

    fbg

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,11 +1,9 @@
 class Square:
     def __init__(self, size=0):
         self.size = size
-
     @property
     def size(self):
         return self._size
-
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -13,10 +11,8 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self._size = value
-
     def area(self):
         return self.size ** 2
-
     def my_print(self):
         if self.size == 0:
             print()
