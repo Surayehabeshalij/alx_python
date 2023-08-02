7c8b39d3f9df258504190078cfe0f293396f53e6
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-02 14:18:03 +0300

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,3 +1,9 @@
 class BaseGeometry:
     def area(self):
         raise Exception("area() is not implemented")
+    __doc__="""
+    this documentation for my method
+    """
+__doc___="""
+this documentation for my class
+"""
