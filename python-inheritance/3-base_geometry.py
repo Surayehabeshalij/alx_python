2b2c1698a0fdf6af16f62ca128ac66e4d672a1e3
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-03 10:19:25 +0300

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """BaseGeometry class Module"""
 class BaseGeometry():
+    def __dir__(self):
+        attrs = dir(type(self))
+        attrs.remove('__init_subclass__')
+        return attrs
     __doc__ ="""
     see work
     """
