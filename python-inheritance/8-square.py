ce960d2da7ba46a04b46f701a909e3b5b3971438
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-03 11:24:01 +0300

    lets

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -10,7 +10,6 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-
     def area(self):
         """Method that returns area of a square"""
         return self.__size ** 2
