5a73a1070e3d92a1725ead9f56c48f62e39eb643
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-03 14:23:32 +0300

    lets

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -11,8 +11,15 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-    def __dir__(cls):
-        attrs = super().__dir__()
-        attrs.remove('__init_subclass__')
-        return attrs
-    
+    def __dir__(cls) -> None:
+        # get list of all attributes for this class and exclude __init_subclass
+        attributes = super().__dir__()
+
+        list_to_return = []
+
+        for attr in attributes:
+            if attr != "__init_subclass__":
+                # append this to the list_to_return
+                list_to_return.append(attr)
+
+        return list_to_return
