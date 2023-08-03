792e5ef166c29db6c8abd576e4b8e1955c89d1b0
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-03 09:46:41 +0300

    see

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """BaseGeometry class Module"""
 class BaseGeometry():
-    def __dir__(cls):
-        attributes = super().__dir__()
+    def __init__(self, x):
+        self.x = x
+    
+    def foo(self):
         pass
+    
+    def __dir__(self):
+        # Get the list of attributes and methods
+        attrs = dir(type(self))
+        # Remove init_subclass from the list
+        attrs.remove("init_subclass")
+        return attrs
     __doc__ ="""
     see work
     """
