db5950edc4a19293d492eaa6536454dfd21ca459
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-03 10:29:05 +0300

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """BaseGeometry class Module"""
 class BaseGeometry():
-    def __getattr__(self, name):
-        if name == '__dir__':
-            attrs = dir(type(self))
-            attrs.remove('__init_subclass__')
-            return lambda: attrs
-        else:
-            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+     def __dir__(self):
+        attrs = dir(super())
+        attrs = dir(type(self))
+        attrs.remove('__init_subclass__')
+        return attrs
     __doc__ ="""
     see work
     """
