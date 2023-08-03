9f83cc2dcf86589b9632ba10022ff3b037317873
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-03 09:16:20 +0300

    here

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """BaseGeometry class Module"""
 class BaseGeometry():
+    def __dir__(self):
+        attributes = super().__dir__()
     """BaseGeometry empty class"""
     pass
