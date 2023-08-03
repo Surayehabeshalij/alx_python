6a45e64b02509f49b6a2936195421a56cc206756
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-03 14:17:22 +0300

    check

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -11,4 +11,8 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+    def __dir__(cls):
+        attrs = super().__dir__()
+        attrs.remove('__init_subclass__')
+        return attrs
     
