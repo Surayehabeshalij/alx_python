1d7eb044cf57f112e7ce216cc14ba63f97f142ea
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-03 11:14:21 +0300

    for

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -4,6 +4,9 @@
 
 class BaseGeometry():
     """BaseGeometry class"""
+    def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
     def area(self):
         """Method Raises an Exception"""
         raise Exception("area() is not implemented")
