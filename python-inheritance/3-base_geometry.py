0510698fb18266f47d622657cb82e3028dab4308
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-03 09:21:29 +0300

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,7 +4,10 @@ class BaseGeometry():
     def __dir__(self):
         attributes = super().__dir__()
         pass
-    """BaseGeometry empty class"""
+    
     ___doc___="""
 BaseGeometry empty class
-"""   
+"""
+__doc__ = """
+pass
+"""
