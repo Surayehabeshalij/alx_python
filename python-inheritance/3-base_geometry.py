39af79b74ca88d348fa45bd2107629d18227c128
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-03 10:33:15 +0300

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,21 +2,21 @@
 """BaseGeometry class Module"""
 class BaseGeometry():
      def __init_subclass__(cls):
-        pass
+          pass
     
-    def __init__(self, x):
-        self.x = x
+     def __init__(self, x):
+          self.x = x
     
-    def foo(self):
-        pass
+     def foo(self):
+          pass
     
-    def __dir__(self):
-        attrs = super().__dir__()
-        attrs.remove('__init_subclass__')
-        return attrs
+     def __dir__(self):
+          attrs = super().__dir__()
+          attrs.remove('__init_subclass__')
+          return attrs
 
-    # Make dir(MyClass) behave the same as dir(MyClass())
-    dir = __dir__
+     # Make dir(MyClass) behave the same as dir(MyClass())
+     dir = __dir__
      __doc__ ="""
     see work
     """
