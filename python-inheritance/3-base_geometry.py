f309e7158dafd4b3f1f554a6906524fdd30b2c79
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-04 10:41:02 +0300

    here

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -4,14 +4,7 @@
 
 class BaseGeometry():
 
-    def __dir__(self) -> Iterable[str]:
-        pass
-    def __dir__(cls) -> None:
-        attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != '__init_subclass__']
-    __doc__ ="""
-    see work
-    """
-__doc__ = """
-pass
-"""
+    
+   __doc__="""
+   this documentation
+   """
