f7e05aa49937a2c0f4d45d80b58a8902e7598b49
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-04 09:58:27 +0300

    here

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """BaseGeometry class Module"""
 class BaseGeometry():
+
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
     def __dir__(cls) -> None:
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
 """Rectangle class Module"""
-from importlib import import_module
-
-BaseGeometry = import_module("5-base_geometry").BaseGeometry
-
+BaseGeometry = __import__("5-base_geometry").BaseGeometry
 class Rectangle(BaseGeometry):
     """Rectangle class"""
-
     def __init__(self, width, height):
-        """Initialize rectangle method"""
+        """Initilize rectangle method"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
-
     def __dir__(self):
-        # Get list of all attributes for this class
-        attributes = super().__dir__()
-
-        # Exclude __init_subclass__ from the list
-        list_to_return = [attr for attr in attributes if attr != "__init_subclass__"]
-
-        return list_to_return
+        attrs = super().__dir__()
+        attrs.remove('__init_subclass__')
+        return attrs
