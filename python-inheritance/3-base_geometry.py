a31ddff57f9151eee063679902f2b22b6f088229
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-04 10:11:22 +0300

    check

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -7,12 +7,3 @@ class BaseGeometry(type):
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
-
-class ObedClass(metaclass=BaseGeometry):
-    """
-    documentation for class goes here
-    """
-    
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
