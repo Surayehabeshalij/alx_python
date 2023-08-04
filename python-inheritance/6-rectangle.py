4cfc198ff796ee6cd62dfd0f968eb5600c74ae42
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-04 09:51:47 +0300

    fine

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """Rectangle class Module"""
-BaseGeometry = __import__("5-base_geometry").BaseGeometry
+from importlib import import_module
+
+BaseGeometry = import_module("5-base_geometry").BaseGeometry
+
 class Rectangle(BaseGeometry):
     """Rectangle class"""
+
     def __init__(self, width, height):
-        """Initilize rectangle method"""
+        """Initialize rectangle method"""
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
         self.__height = height
+
     def __dir__(self):
-        attrs = super().__dir__()
-        attrs.remove('__init_subclass__')
-        return attrs
+        # Get list of all attributes for this class
+        attributes = super().__dir__()
+
+        # Exclude __init_subclass__ from the list
+        list_to_return = [attr for attr in attributes if attr != "__init_subclass__"]
+
+        return list_to_return
