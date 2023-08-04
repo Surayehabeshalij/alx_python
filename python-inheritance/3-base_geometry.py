b5d02221e09e5ab32f1d3ffa06a3b27cf5daffb4
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-04 10:12:14 +0300

    run

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """BaseGeometry class Module"""
 class BaseGeometry(type):
+    
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     """
     documentation
     """
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
