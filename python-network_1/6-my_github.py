e23ecb3fe9c762e85964c93bdfffbeb3adf7b910
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 15:04:51 +0300

    here

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -2,11 +2,11 @@
 """takes your GitHub credentials (username and password) and uses the GitHub
 API to display your id"""
 import requests
-import sys 
+from sys import argv
 
 
 if __name__ == '__main__':
-    r = requests.get('https://api.github.com/Surayehabeshalij', auth=(sys.argv[1], sys.argv[2]))
+    r = requests.get('https://api.github.com/user', auth=(argv[1], argv[2]))
     try:
         print(r.json().get('id'))
     except ValueError:
