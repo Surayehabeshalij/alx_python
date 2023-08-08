8fd31e561fd79827ec0ae1ac648af5b59dc1dafd
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 13:58:28 +0300

    here

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""takes your GitHub credentials (username and password) and uses the GitHub
-API to display your id"""
+""" Github code challenge"""
 import requests
 from sys import argv
 
-
-if __name__ == '__main__':
-    r = requests.get('https://api.github.com/Surayehabeshalij', auth=(argv[1], argv[2]))
-    try:
-        print(r.json().get('id'))
-    except ValueError:
-        print('Not a valid JSON')
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits"\
+          .format(argv[2], argv[1])
+    r = requests.get(url)
+    n = 0
+    for i in r.json():
+        if n < 10:
+            print("{}: {}".format(i.get("sha"),
+                  i.get("commit").get("author").get("name")))
+        n += 1
