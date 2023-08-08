20ccaffde3227b59b98e181dc56613a1fed1e603
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 14:18:25 +0300

    here

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,29 +1,11 @@
+#!/usr/bin/python3
+"""
+Python script that fetches an URL with requests package
+"""
 import requests
 
-url = "https://alu-intranet.hbtn.io/status"
 
-response = requests.get(url)
-
-expected_response = {
-    "type": "<class 'str'>",
-    "content": "OK",
-    "length": 2
-}
-
-actual_response = {
-    "type": str(type(response.text)),
-    "content": response.text,
-    "length": len(response.text)
-}
-
-print("[Expected]")
-print("Body response:")
-print(f"\t- type: {expected_response['type']}")
-print(f"\t- content: {expected_response['content']}")
-print(f"\t- length: {expected_response['length']}\n")
-
-print("[Got]")
-print("Body response:")
-print(f"\t- type: {actual_response['type']}")
-print(f"\t- content: {actual_response['content']}")
-print(f"\t- length: {actual_response['length']}")
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    t = r.text
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
