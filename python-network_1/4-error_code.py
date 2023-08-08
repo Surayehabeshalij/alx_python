000f8688c95778f2e410569c4bad9016e6e4c752
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 15:23:29 +0300

    here

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""takes in a URL, sends a request to the URL and displays the body
-of the response"""
-import requests
+"""Sends a request to a given URL and displays the response body.
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
 import sys
+import requests
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please provide a URL as a command-line argument")
-        sys.exit(1)
 
+if __name__ == "__main__":
     url = sys.argv[1]
+
     r = requests.get(url)
-    
     if r.status_code >= 400:
-        print('Error code:', r.status_code)
+        print("Error code: {}".format(r.status_code))
     else:
         print(r.text)
