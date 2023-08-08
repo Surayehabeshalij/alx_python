f7bd263605b7be8060eb9eb2006738b0b7456abc
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 14:45:00 +0300

    here

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
-import requests
+"""Takes URL sends request and displays value of X-Request-Id"""
 import urllib.request
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        with urllib.request.urlopen(sys.argv[1]) as response:
-            print(response.getheader("X-Request-Id"))
-        print("Please provide a URL as a command-line argument")
-        sys.exit(1)
-
-    url = sys.argv[1]
-    response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')
-    print(f"X-Request-Id: {request_id}")
+if len(argv) > 1:
+    with urllib.request.urlopen(argv[1]) as response:
+        print(response.getheader("X-Request-Id"))
