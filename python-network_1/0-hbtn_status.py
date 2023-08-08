09e8e7e3f302b293084759ff32fb3f1eacbf4b08
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 14:35:54 +0300

    true

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""
-Python script that fetches an URL with requests package
-"""
-import requests
+"""Fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request
 
 
 if __name__ == "__main__":
-    r = requests.get('https://alx-intranet.hbtn.io/status')
-    t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,10 +3,13 @@
 Python script that fetches an URL with requests package
 """
 import requests
+import urllib.request
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.getheader("X-Request-Id"))
         print("Please provide a URL as a command-line argument")
         sys.exit(1)
 
