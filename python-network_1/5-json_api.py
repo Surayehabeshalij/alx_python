ac912bfb1c5981e982ad843f22d6934046b90298
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-08 15:10:04 +0300

    here

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -2,11 +2,11 @@
 """takes letter and sends a POST req to http://0.0.0.0:5000/search_user
 with the letter as a parameter."""
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == '__main__':
-    letter = {'q': argv[1][0] if len(argv) > 1 else ''}
+    letter = {'q': sys.argv[1][0] if len(sys.argv) > 1 else ''}
     r = requests.post('http://0.0.0.0:5000/search_user', data=letter)
     try:
         response = r.json()
