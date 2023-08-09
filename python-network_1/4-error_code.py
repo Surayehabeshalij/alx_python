d0008b8008f50f77e3b693282940f69ea017ee43
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-09 11:46:19 +0300

    here

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -2,14 +2,12 @@
 """takes url & email, sends a POST request and displays the response"""
 import requests
 import sys
-from urllib.request import Request, urlopen
-from urllib.error import HTTPError
 
 if __name__ == '__main__':
-    req = Request(sys.argv[1])
-    try:
-        with urlopen(req) as response:
-            r = response.read()
-            print(r.decode('utf-8'))
-    except HTTPError as e:
-        print('Error code:', e.code)
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print('Error code: {}'.format(response.status_code))
