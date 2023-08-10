e0da9fb6598bd97abc9b3c2b5932aa3b18828e09
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 10:55:31 -0700

    check

--- python-network_1/5-json_api.py
+++ python-network_1/5-json_api.py
@@ -1,24 +1,26 @@
-#!/usr/bin/python3
-"""
-    Script that takes in a letter and sends a POST request to
-    https://0.0.0.0:5000/search_user with the letter as a parameter
-    the letter is sent in variable q, otherwise q is empty
-    displays [<id>] <name> if repsonse is properly JSON formatted
-"""
 import requests
 import sys
 
 def send_post_request(url, letter):
     data = {'q': letter}
     response = requests.post(url, data=data)
-    return response.json()
+
+    try:
+        response_json = response.json()
+    except ValueError:
+        # Invalid JSON response
+        return None
+
+    return response_json
 
 if __name__ == '__main__':
     url = 'http://0.0.0.0:5000/search_user'
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
     response_json = send_post_request(url, letter)
 
-    if isinstance(response_json, dict):
+    if response_json is None:
+        print("Not a valid JSON")
+    elif isinstance(response_json, dict):
         if 'id' in response_json and 'name' in response_json:
             print("[{}] {}".format(response_json['id'], response_json['name']))
         elif 'message' in response_json:
