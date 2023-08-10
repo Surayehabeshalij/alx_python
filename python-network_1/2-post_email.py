a5602fcaadf51d2c5eb513c6b72427a2feab07a7
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 11:08:33 -0700

    one

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,19 +1,17 @@
-#!/usr/bin/python3
-"""takes url & email, sends a POST request and displays the response"""
 import requests
 import sys
 
 if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-    pyload= {
+    payload = {
         "email": email
     }
-    response = requests.post(url, data=pyload)
+    response = requests.post(url, params=payload)
     try:
         json = response.json()
         if json:
-            get_email = json.get("form")["email"]    
-            print("your email is:" get_email)
+            get_email = json.get("form")["email"]
+            print("Your email is:", get_email)
     except ValueError as e:
         print(e)
