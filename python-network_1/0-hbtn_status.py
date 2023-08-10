7877b515a164d540c6613dbe518d4738c89bdbcc
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 12:37:34 -0700

    here

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,10 +2,9 @@
 """Fetches https://alx-intranet.hbtn.io/status."""
 import requests
 
-ur1 = "https://alx-intranet.hbtn.io/status"
+ur1 = "https://intranet.hbtn.io/status"
 response= requests.get(ur1)
 if response.status_code == 200:
-    r = requests.get("https://alx-intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: ", type(response.text))
     print("\t- content: " , response.text)
