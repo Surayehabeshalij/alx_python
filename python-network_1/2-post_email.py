ad2f50fc999c74c63a6cc5dd41e049d239adffe7
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 12:03:42 -0700

    here

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,17 +1,16 @@
+#!/usr/bin/python3
+"""Displays the X-Request-Id header variable of a request to a given URL.
+Usage:  <URL>
+"""
+"""Takes URL, sends a request and displays the value of X-Request-Id"""
 import requests
 import sys
 
+def main():
+    url=sys.argv[1]
+    email=sys.argv[2]
+    r= requests.post(url, data={'email':email})
+    r1=r.text
+    print(r1)
 if __name__ == '__main__':
-    url = sys.argv[1]
-    email = sys.argv[2]
-    payload = {
-        "email": email
-    }
-    response = requests.post(url, params=payload)
-    try:
-        json = response.json()
-        if json:
-            get_email = json.get("form")["email"]
-            print("Your email is:", get_email)
-    except ValueError as e:
-        print(e)
+    main()
