84e08037254191263a5c49cf8b438e367319816e
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 12:58:57 -0700

    last

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
 
     if response.status_code >= 400:
         print('Error code:', response.status_code)
-    else: response.status_code < 400
-    print('Error code:', response.status_code)
+    else:
+        print(response.text)
