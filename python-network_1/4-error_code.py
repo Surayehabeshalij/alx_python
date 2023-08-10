eaef6ff074bb8498444cbac0a660ecd73a874f19
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 12:50:58 -0700

    go

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     url = sys.argv[1]
     response = requests.get(url)
 
-    if response.status_code >= 400:
+    if response.status_code <= 400:
         print('Error code:', response.status_code)
-    elif response.status_code <= 400:
+    elif response.status_code > 400:
         print('Error code:', response.status_code)
     else:
         print(response.text)
