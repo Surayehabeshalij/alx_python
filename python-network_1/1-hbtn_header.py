57d993e2d5e791fb27977094b7f082da07f53b50
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 12:05:51 +0300

    here

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -2,12 +2,24 @@
 """Displays the X-Request-Id header variable of a request to a given URL.
 Usage:  <URL>
 """
-from sys import argv
 import requests
+import sys
 
+def retrieve_x_request_id(url):
+    try:
+        response = requests.get(url)
+        x_request_id = response.headers.get('X-Request-Id')
+        if x_request_id:
+            print(f'The value of X-Request-Id is: {x_request_id}')
+        else:
+            print('X-Request-Id header not found in the response.')
+    except requests.exceptions.RequestException as e:
+        print(f'An error occurred: {e}')
 
-if __name__ == "__main__":
-    url = argv[1]
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Please provide a URL as an argument.')
+        sys.exit(1)
 
-    r = requests.get(url)
-    print(r.headers.get("X-Request-Id"))
+    url = sys.argv[1]
+    retrieve_x_request_id(url)
