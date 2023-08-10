3993a9d400f52d3dc78d88c10325bb2597db406d
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-10 11:44:24 -0700

    check

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -2,17 +2,13 @@
 """takes in a URL, sends a request to the URL and displays the body
 of the response"""
 import requests
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    url = argv[1]
-    r = requests.get(url)
-    if r.status_code == 200:
-        print('Correct output - case: request {} with status code 200'.format(url))
-        print(r.text)
-    elif r.status_code == 401:
-        print('Correct output - case: request {} with status code 401'.format(url))
-    elif r.status_code == 500:
-        print('Correct output - case: request {} with status code 500'.format(url))
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print('Error code:', response.status_code)
     else:
-        print('Error code:', r.status_code)
+        print(response.text)
