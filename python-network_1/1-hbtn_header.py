1184bf80e7b167a145d8dbf4d6aaa57aab8723ae
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-10 15:21:19 +0300

    Update 1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -8,19 +8,18 @@ import sys
 def retrieve_x_request_id(url):
     try:
         response = requests.get(url)
-        response.raise_for_status()  # Check for HTTP errors
         x_request_id = response.headers.get('X-Request-Id')
         if x_request_id:
             print(f'The value of X-Request-Id is: {x_request_id}')
         else:
             print('X-Request-Id header not found in the response.')
     except requests.exceptions.RequestException as e:
-        print(f'An error occurred: {e}')
+        print(f'An error occurredd: {e}')
 
 if __name__ == '__main__':
-    """if len(sys.argv) < 2:
+    if len(sys.argv) < 2:
         print('Please provide a URL as an argument.')
-        sys.exit(1)"""
+        sys.exit(1)
 
     url = sys.argv[1]
     retrieve_x_request_id(url)
