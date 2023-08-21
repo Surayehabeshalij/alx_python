9af6c028d1eb1908089f0ee8ba6c63d84e741183
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-21 10:06:26 +0300

    Update 3-my_safe_filter_states.py

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 '''
-script that takes in an argument and displays all values in the states
-'''
+script that takes in an argument and displays all values in the states'''
 import MySQLdb
 import sys
 if __name__ == '__main__':
