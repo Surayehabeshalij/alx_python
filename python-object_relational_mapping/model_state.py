249240b6e45f12a29ef2243abc374a2906101ec4
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-08-21 10:29:27 +0300

    Update model_state.py

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -5,6 +5,8 @@ from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
+"""
+Class with id and name attributes of each state """
 class State(Base):  
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
