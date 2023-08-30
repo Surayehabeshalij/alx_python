0f00bfd7d82806bdb3d619ab9c5dd3260793e73e
Surayehabeshalij <surafelshemsu@gmail.com>
2023-08-30 13:55:04 +0300

    final

--- python-web_framework/6-number_odd_or_even.py
+++ python-web_framework/6-number_odd_or_even.py
@@ -34,7 +34,7 @@ def display_number_template(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even_template/<int:n>', strict_slashes=False)
 def display_number_odd_or_even(n):
     return render_template('6-number_odd_or_even.html', n=n, parity='even' if n % 2 == 0 else 'odd')
 '''
