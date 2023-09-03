a08e27745003be1210d7d48468189f8357d25a81
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-03 03:07:28 -0700

    Update 3-main.py

--- python-inheritance/3-main.py
+++ python-inheritance/3-main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 BaseGeometry = __import__('3-base_geometry').BaseGeometry
-
+def __dir__(cls) -> None:
+        attributes = super().__dir__()
+        return [attribute for attribute in attributes if attribute != '__init_subclass__']
 bg = BaseGeometry()
 
 print(bg)
