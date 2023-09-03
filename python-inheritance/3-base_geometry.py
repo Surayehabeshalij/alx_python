c7404d015b2797d1ff7dd11e79afbbf3d4e1cf7b
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-03 03:15:30 -0700

    Update 3-base_geometry.py

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,6 +6,6 @@ class BaseGeometry:
     """This is an empty class"""
     pass
      """This is an empty class"""
-     def __dir__(cls) -> None:
+    def __dir__(cls) -> None:
         attributes = super().__dir__()
         return [attribute for attribute in attributes if attribute != '__init_subclass__']
