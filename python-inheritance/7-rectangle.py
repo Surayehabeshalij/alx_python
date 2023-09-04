4764ad74ad40d0e52ab4a03a98be388fa8756cd5
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-04 14:09:16 +0300

    Update 7-rectangle.py

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,26 +1,62 @@
-#!/usr/bin/python3
-"""Rectangle class Module"""
-BaseGeometry = __import__("5-base_geometry").BaseGeometry
+""" parent class: BaseGeometry
+    child class: Rectangle"""
 
+class BaseGeometry(type):
+    """ every thing in python is an object soo the class is also an object.
+    so if it is an object it have type.type is a metaclass, of which classes
+    are instances. Just as an ordinary object is an instance of a class,
+    any new-style class in Python, and thus any class in Python 3,
+    is an instance of the type metaclass """
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
 
-class Rectangle(BaseGeometry):
-    """Rectangle class"""
-    def __init__(self, width, height):
-        """Initilize rectangle method"""
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+class BaseGeometry(metaclass=BaseGeometry):
+
+    """ Metaclasses allow for code to manipulate classes.
+    Often this change happens when an object of the class is instantiated
+    """
+
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
 
     def area(self):
-        """Method that returns area of rectangle"""
+        """ raises an Exception with the message """
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        self.__name = name
+        self.__value = value
+        if isinstance(value, int):
+            if value > 0:
+                self.__value = value
+            else:
+                raise ValueError("{} must be greater than 0".format(self.__name))
+        else:
+            raise TypeError("{} must be an integer".format(self.__name))
+
+class Rectangle(BaseGeometry):
+    """" the class inherit from BaseGeometry"""
+    def __init__(self, width, height):
+        """" validet width and height from parent class """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+    def area(self):
+        """" return the area of the rectangle"""
         return self.__width * self.__height
-
     def __str__(self):
-        """Returns a string"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-    def __dir__(cls) -> None:
-        attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != '__init_subclass__']
-
-   
+        """ returns the object representation in a string format """
+        return("[Rectangle] {}/{}".format(self.__width, self.__height))
