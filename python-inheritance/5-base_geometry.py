1902ebb068551f8f8490e6e66ab2868f90956404
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-04 14:10:51 +0300

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,19 +1,46 @@
-#!/usr/bin/python3
-"""BaseGeometry class Module"""
+""" class: BaseGeometry """
 
-
-class BaseGeometry():
-    """BaseGeometry class"""
-    def __dir__(cls) -> None:
+class BaseGeometry(type):
+    """ every thing in python is an object soo the class is also an object.
+    so if it is an object it have type.type is a metaclass, of which classes
+    are instances. Just as an ordinary object is an instance of a class, 
+    any new-style class in Python, and thus any class in Python 3, 
+    is an instance of the type metaclass """
+    def __dir__(cls):
+        """ remove the init subclass"""
         attributes = super().__dir__()
-        return [attribute for attribute in attributes if attribute != '__init_subclass__']
-    def area(self):
-        """Method Raises an Exception"""
-        raise Exception("area() is not implemented")
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
 
+class BaseGeometry(metaclass=BaseGeometry):
+
+    """ Metaclasses allow for code to manipulate classes.
+    Often this change happens when an object of the class is instantiated
+    """
+
+    def __dir__(cls):
+        """ remove the init subclass"""
+        attributes = super().__dir__()
+        list_to_return = []
+        for att in attributes:
+            if att != "__init_subclass__":
+                list_to_return.append(att)
+        return list_to_return
+
+    def area(self):
+        """ raises an Exception with the message """
+        raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
-        """Method for validating value"""
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+        self.__name = name
+        self.__value = value
+        if isinstance(value, int):
+            if value > 0:
+                self.__value = value
+            else:
+                raise ValueError("{} must be greater than 0".format(self.__name))
+        else:
+            raise TypeError("{} must be an integer".format(self.__name))
