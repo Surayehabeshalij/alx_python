14d56a1dabd35c1351af0ff38bc1061d710bba53
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-04 13:51:44 +0300

    Update 6-rectangle.py

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -6,5 +6,13 @@ BaseGeometry = __import__('5-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """" the class inherit from BaseGeometry"""
+        def __init__(self, width, height):
+        """" validet width and height from parent class """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+
+
 
    
