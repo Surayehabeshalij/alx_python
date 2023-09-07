6b241d6a4fadee66da265cce42dc05e770e88366
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-07 11:12:35 +0300

    Update 4-error_code.py

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 """takes in a URL, sends a request to the URL and displays the body
 of the response"""
-import requests
-import sys
+#!/usr/bin/python3
+"""Send a request to the url and print the response"""
 
-if __name__ == '__main__':
-    url = sys.argv[1]
-    response = requests.get(url)
+from requests import get
+from sys import argv
 
-    if response.status_code >= 400:
-        print('Error code:', response.status_code)
-    else:
-        print(response.text)
+
+def request_header_property(url: str) -> str:
+    """
+    Send a request to the URL specified and
+    get the response and handle exceptions
+    Args:
+        url (str): The URL to query
+    """
+    response = get(url)
+    if int(response.status_code) >= 400:
+        return ("Error code: {}".format(response.status_code))
+
+    return response.text
+
+
+if __name__ == "__main__":
+    print(request_header_property(argv[1]))
