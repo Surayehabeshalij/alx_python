d2b117222ca40ec001147b135df2709f9a13a911
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-25 11:07:54 +0300

    Update 8-square.py

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,61 +1,18 @@
 #!/usr/bin/python3
 """Defines a base geometry class BaseGeometry."""
+"""
+parent class: BaseGeometry
+child class: Rectangle
+"""
 
-
-class baseGeo(type):
-    """The custom metaclass"""
-    def __dir__(self):
-        """Overrides dir() method for the metaclass"""
-        return [attribute for attribute in super().__dir__()
-                if attribute != '__init_subclass__']
-
-
-class BaseGeometry(metaclass=baseGeo):
-    """Reprsent base geometry."""
-
-    def area(self):
-        """Not yet implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validate a parameter as an integer
-            TypeError: If value is not an integer.
-            ValueError: If value is <= 0.
-        """
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-        else:
-            return value
-
-    def __dir__(self):
-        """Overrides dir() method for the metaclass"""
-        return [attribute for attribute in super().__dir__()
-                if attribute != '__init_subclass__']
-
-
-class Rectangle(BaseGeometry):
-    """rectangle class"""
-
-    def __init__(self, width, height):
-        """initializing width and height"""
-        self.__width = super().integer_validator("width", width)
-        self.__height = super().integer_validator("height", height)
-
-    def __str__(self):
-        """str method"""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def area(self):
-        """rectangle area"""
-        return self.__width * self.__height
-
+Rectangle = __import__('7-rectangle').Rectangle
 
 class Square(Rectangle):
-    """Square class"""
-
+    """ the class inherite from rectangle """
     def __init__(self, size):
-        """initializing square size"""
-        self.__size = super().integer_validator("size", size)
+        """use the rectangle methods"""
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(size, size)
+        self.__str__()
+        self.area()
