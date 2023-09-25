80862bb64f19fc1e09612db6a9928b6d79459d8b
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-25 11:09:23 +0300

    Update 2-inherits_from.py

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,14 +1,10 @@
-#!/usr/bin/python3
-"""Defines an inherited class"""
+""" if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class """
 
 
 def inherits_from(obj, a_class):
-    """Checks if an object is an inherited."""
-
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
+    """ checking the condition """
+    if isinstance(obj, a_class) and type(obj) != a_class:
         return True
-    return False
-
-
-""" if the object is an instance of a class that inherited
-(directly or indirectly) from the specified class """
+    else:
+        return False
