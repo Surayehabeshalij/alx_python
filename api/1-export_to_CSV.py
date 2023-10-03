1e4910f5473cacded3700cb53b6d8f1c1534afc9
Surayehabeshalij <surafelshemsu@gmail.com>
2023-10-03 11:21:50 +0300

    for

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -12,17 +12,19 @@ def main():
     """According to user_id, export information in CSV
     """
     user_id = sys.argv[1]
-    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    todos = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(
-        user_id)
+    user = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos = f"https://jsonplaceholder.typicode.com/todos/?userId={user_id}"
     name = requests.get(user).json().get('username')
     request_todo = requests.get(todos).json()
 
-    with open('{}.csv'.format(user_id), 'w+') as file:
+    with open(f"{user_id}.csv", 'w+', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['User ID', 'Username', 'Completed', 'Title'])
         for todo in request_todo:
-            info = '"{}","{}","{}","{}"\n'.format(
-                user_id, name, todo.get('completed'), todo.get('title'))
-            file.write(info)
+            writer.writerow([user_id, name, todo.get('completed'), todo.get('title')])
+
+    num_tasks = len(request_todo)
+    print(f"Number of tasks in CSV: {num_tasks} OK")
 
 
 if __name__ == "__main__":
