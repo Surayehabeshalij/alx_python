398485587ac2d61e3724c0e41b2a56f9340f8f8c
Surayehabeshalij <surafelshemsu@gmail.com>
2023-10-03 14:19:19 +0300

    here

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -30,3 +30,25 @@ if __name__ == "__main__":
               mode='w', encoding='utf-8') as file_open:
         f = json.dumps(task_list)
         file_open.write(f)
+def user_info():
+    """ Check user info """
+    
+    with open('todo_all_employees.json', 'r') as f:
+        student_json = json.load(f)
+
+    correct_json = requests.get(users_url).json()
+
+    for correct_entry in correct_json:
+        flag = 0
+        for student_entry in student_json:
+            if str(correct_entry['id']) == student_entry:
+                flag = 1
+        if flag == 0:
+            print("User ID {} Found: Incorrect".format(correct_entry['id']))
+            return
+    
+    print("All users found: OK")
+
+
+if __name__ == "__main__":
+    user_info()
