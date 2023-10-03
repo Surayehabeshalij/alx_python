a0ed607d047c73b9f65b5ab232b5e580c13ce51a
Surayehabeshalij <surafelshemsu@gmail.com>
2023-10-03 12:27:42 +0300

    for

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -8,29 +8,57 @@ import requests
 import sys
 
 
-def get_todo_info():
-    """A function that gets the todo information for a particular user id"""
-    user_id = sys.argv[1]
-    # GET /user/<id> resource for user info
-    r = requests.get('https://jsonplaceholder.typicode.com/users?id={}'
-                     .format(user_id))
-    user = json.loads(r.text)
-    user_name = user[0].get('username')
+def get_employee_data(employee_id):
+    # Define the base URL for the JSONPlaceholder API
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    # GET /user/<id>/todos for todo info
-    r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                     .format(user_id))
-    todos = json.loads(r.text)
+    # Construct the URLs for employee details and TODO list
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/users/{employee_id}/todos"
 
-    with open('{}.csv'.format(user_id),
-              'w', newline='', encoding='utf-8') as fp:
-        taskwriter = csv.writer(fp, quoting=csv.QUOTE_ALL)
-        for task in todos:
-            taskwriter.writerow(["{}".format(user_id),
-                                 "{}".format(user_name),
-                                 "{}".format(task.get('completed')),
-                                 "{}".format(task.get('title'))])
+    # Fetch employee details
+    try:
+        response = requests.get(employee_url)
+        response.raise_for_status()
+        employee_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching employee details: {e}")
+        sys.exit(1)
 
+    # Fetch TODO list
+    try:
+        response = requests.get(todo_url)
+        response.raise_for_status()
+        todo_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching TODO list: {e}")
+        sys.exit(1)
+
+    return employee_data, todo_data
+
+def export_to_csv(employee_data, todo_data):
+    # Extract relevant information
+    employee_id = employee_data["id"]
+    employee_name = employee_data["username"]
+
+    # Create a CSV file with the employee ID as the filename
+    filename = f"{employee_id}.csv"
+    with open(filename, mode="w", newline="") as csv_file:
+        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)  # Quote all fields
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in todo_data:
+            csv_writer.writerow([str(employee_id), employee_name, str(task["completed"]), task["title"]])
 
 if __name__ == "__main__":
-    get_todo_info()
+    if len(sys.argv) != 2:
+        print("Usage: python export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    employee_data, todo_data = get_employee_data(employee_id)
+    export_to_csv(employee_data, todo_data)
