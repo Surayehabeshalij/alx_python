64eba4266259a8e7d82482ce79e15c638be51dba
Surayehabeshalij <surafelshemsu@gmail.com>
2023-10-05 09:19:48 +0300

    for csv file

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -7,29 +7,68 @@ import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    employee_id = sys.argv[1]
-    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
-    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
-    data = api_request.text
-    pjson = json.loads(data)
-    data1 = api_request1.text
-    pjson1 = json.loads(data1)
+def get_employee_data(employee_id):
+    # Define the base URL for the JSONPlaceholder API
+    base_url = "https://jsonplaceholder.typicode.com"
 
-    name_info = pjson['name']
+    # Construct the URLs for employee details and TODO list
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/users/{employee_id}/todos"
 
-    filename = "{}.json".format(employee_id)
+    # Fetch employee details
+    try:
+        response = requests.get(employee_url)
+        response.raise_for_status()
+        employee_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching employee details: {e}")
+        sys.exit(1)
 
-    # Create the dictionary structure
-    result = {
-        employee_id: [{
-            "task": item["title"],
-            "completed": item["completed"],
-            "username": name_info
-        } for item in pjson1]
+    # Fetch TODO list
+    try:
+        response = requests.get(todo_url)
+        response.raise_for_status()
+        todo_data = response.json()
+    except requests.exceptions.RequestException as e:        
+        print(f"Error fetching TODO list: {e}")
+        sys.exit(1)
+
+    return employee_data, todo_data
+
+def export_todo_progress_to_json(employee_data, todo_data, employee_id):
+    # Extract relevant information
+    employee_name = employee_data.get("name")
+    tasks = []
+
+    for task in todo_data:
+        tasks.append({
+            "task": task["title"],
+            "completed": task["completed"],
+            "username": employee_data["username"]
+        })
+
+    # Create the JSON data
+    json_data = {
+        str(employee_id): tasks
     }
 
-    # export data to json file
+    # Write the JSON data to a file
+    file_name = f"{employee_id}.json"
+    with open(file_name, "w") as json_file:
+        json.dump(json_data, json_file, indent=4)
 
-    with open(filename, "w") as outfile:
-        json.dump(result, outfile)
+    print(f"Data exported to {file_name}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python gather_data_and_export_to_JSON.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
+
+    employee_data, todo_data = get_employee_data(employee_id)
+    export_todo_progress_to_json(employee_data, todo_data, employee_id)
