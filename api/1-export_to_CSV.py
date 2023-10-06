8c55a32d07b459720bcd59715aafc03f9d329129
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-10-06 01:03:02 -0700

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,70 +4,27 @@ Write a Python script that, using this REST API,
 for a given employee ID, returns information about
 his/her TODO list progress and exports it in CSV format.
 """
-
-import pandas as pd
+import csv
+import json
 import requests
 import sys
 
-def get_employee_data(employee_id):
-    # Define the base URL for the JSONPlaceholder API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Construct the URLs for employee details and TODO list
-    employee_url = f"{base_url}/users/{employee_id}"
-    todo_url = f"{base_url}/users/{employee_id}/todos"
-
-    # Fetch employee details
-    try:
-        response = requests.get(employee_url)
-        response.raise_for_status()
-        employee_data = response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Error fetching employee details: {e}")
-        sys.exit(1)
-
-    # Fetch TODO list
-    try:
-        response = requests.get(todo_url)
-        response.raise_for_status()
-        todo_data = response.json()
-    except requests.exceptions.RequestException as e:        
-        print(f"Error fetching TODO list: {e}")
-        sys.exit(1)
-
-    return employee_data, todo_data
-
-def export_to_csv(employee_data, todo_data):
-    # Extract relevant information
-    user_id = employee_data.get("id")
-    username = employee_data.get("username")
-
-    # Create a DataFrame for the TODO list
-    todo_df = pd.DataFrame(todo_data)
-
-    # Select relevant columns and add user-specific data
-    todo_df = todo_df[["completed", "title"]]
-    todo_df["user_id"] = user_id
-    todo_df["username"] = username
-
-    # Rename columns
-    todo_df = todo_df.rename(columns={"completed": "TASK_COMPLETED_STATUS", "title": "TASK_TITLE"})
-
-    # Save to CSV
-    csv_filename = f"{user_id}.csv"
-    todo_df.to_csv(csv_filename, index=False)
-    print(f"Data exported to {csv_filename}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    employee_id = sys.argv[1]
+    api_request = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employee_id))
+    api_request1 = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id))
+    data = api_request.text
+    pjson = json.loads(data)
+    data1 = api_request1.text
+    pjson1 = json.loads(data1)
 
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError:
-        print("Employee ID must be an integer.")
-        sys.exit(1)
-
-    employee_data, todo_data = get_employee_data(employee_id)
-    export_to_csv(employee_data, todo_data)
+    #export data to csv data
+    filename = "{}.csv".format(employee_id)
+    with open(filename, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
+        for item in pjson1:
+            user_id = employee_id
+            username = pjson['username']
+            task_completed_status = item['completed']
+            task_title = item['title']
+            writer.writerow([user_id, username, task_completed_status, task_title])
