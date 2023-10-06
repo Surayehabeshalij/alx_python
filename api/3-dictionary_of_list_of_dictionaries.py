8e21f5c62a7ace17890a9e946ded42cd9e4744ed
Surayehabeshalij <surafelshemsu@gmail.com>
2023-10-06 02:40:37 -0700

    updating api

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -2,31 +2,83 @@
 '''
 A script to export data in the JSON format
 '''
+#!/usr/bin/python3
+"""
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about
+his/her TODO list progress
+"""
+
 import json
 import requests
+import sys
+
+def get_employee_data(employee_id):
+    # Define the base URL for the JSONPlaceholder API
+    base_url = "https://jsonplaceholder.typicode.com"
+
+    # Construct the URLs for employee details and TODO list
+    employee_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/users/{employee_id}/todos"
+
+    # Fetch employee details
+    try:
+        response = requests.get(employee_url)
+        response.raise_for_status()
+        employee_data = response.json()
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching employee details: {e}")
+        sys.exit(1)
+
+    # Fetch TODO list
+    try:
+        response = requests.get(todo_url)
+        response.raise_for_status()
+        todo_data = response.json()
+    except requests.exceptions.RequestException as e:        
+        print(f"Error fetching TODO list: {e}")
+        sys.exit(1)
+
+    return employee_data, todo_data
+
+def export_todo_data(todo_data):
+    employee_todo_dict = {}
+    for task in todo_data:
+        user_id = task["userId"]
+        if user_id not in employee_todo_dict:
+            employee_todo_dict[user_id] = []
+        
+        employee_todo_dict[user_id].append({
+            "username": task["title"],
+            "task": task["title"],
+            "completed": task["completed"]
+        })
+
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(employee_todo_dict, json_file, indent=4)
+
+def display_todo_progress(employee_data, todo_data):
+    # Extract relevant information
+    employee_name = employee_data.get("name")
+    completed_tasks = [task for task in todo_data if task["completed"]]
+    total_tasks = len(todo_data)
+
+    # Display employee TODO list progress
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task['title']}")
 
 if __name__ == "__main__":
-    api_request_users = requests.get("https://jsonplaceholder.typicode.com/users")
-    api_request_todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    users_data = api_request_users.json()
-    todos_data = api_request_todos.json()
+    if len(sys.argv) != 2:
+        print("Usage: python gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
 
-    filename = "todo_all_employees.json"
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Employee ID must be an integer.")
+        sys.exit(1)
 
-    result = {}
-    for user in users_data:
-        user_id = user["id"]
-        username = user["username"]
-        user_todos = [
-            {
-                "task": todo["title"],
-                "completed": todo["completed"],
-                "username": username
-            }
-            for todo in todos_data
-            if todo["userId"] == user_id
-        ]
-        result[user_id] = user_todos
-
-    with open(filename, "w") as outfile:
-        json.dump(result, outfile)
+    employee_data, todo_data = get_employee_data(employee_id)
+    display_todo_progress(employee_data, todo_data)
+    export_todo_data(todo_data)
